a162d8a4f118c86f82ed75acffbd54520a8e6ada
rafaeldaro <50755442+rafaeldaro@users.noreply.github.com>
2019-10-01 19:53:39 -0300

    intervalo

--- index.php
+++ index.php
@@ -41,16 +41,16 @@
         <section class="row">
             <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
                 <h2 class="text-center mt-2">
-                    Entrar no Site.
+                    Entrar no Sistema.
                 </h2>
                 <form action="#" method="post" class="p-2" id="formLogin">
 
                     <div class="form-group">
-                        <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome de Usuário" class="form-control">
+                        <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome de Usuário" class="form-control" required minlength="5">
                     </div>
 
                     <div class="form-group">
-                        <input type="password" name="senhausuario" id="senhausuario" placeholder="Senha " class="form-control">
+                        <input type="password" name="senhausuario" id="senhausuario" placeholder="Senha " class="form-control" required minlength="5">
                     </div>
 
 
@@ -78,8 +78,8 @@
                         </p>
                     </div>
 
-                </form>
-            </div>
+                    <!-- </form>
+            </div> -->
         </section>
         <!-- final da seção de Login -->
         <br>
@@ -97,7 +97,7 @@
                     </div>
 
                     <div class="form-group">
-                        <input type="email" name="emailGerarSenha" id="emailGerarsenha" class="form-control" placeholder="E-mail de recuperação de senha ">
+                        <input type="email" name="emailGerarSenha" id="emailGerarsenha" class="form-control" placeholder="E-mail de recuperação de senha " required>
                     </div>
 
                     <div class="form-group">
@@ -128,11 +128,11 @@
                 <form action="#" method="post" class="p-2" id="formRegistro">
 
                     <div class="form-group">
-                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder=" Nome completo">
+                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder=" Nome completo" required minlength="6">
                     </div>
 
                     <div class="form-group">
-                        <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder=" Nome de usuário">
+                        <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder=" Nome de usuário" required minlength="5">
                     </div>
 
                     <div class="form-group">
@@ -143,13 +143,13 @@
 
                     <div class="form-group">
 
-                        <input type="password" name="senhaUsuario" id="senhaUsuario" class="form-control" placeholder="Senha">
+                        <input type="password" name="senhaDoUsuario" id="senhaDoUsuario" class="form-control" placeholder="Senha" required minlength="6">
 
                     </div>
 
                     <div class="form-group">
 
-                        <input type="password" name="senhaUsuarioConfirmar" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirmar Senha">
+                        <input type="password" name="senhaUsuarioConfirmar" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirmar Senha" required minlength="6">
 
                     </div>
 
@@ -200,10 +200,10 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"> </script>
         //código jQuery para mostrare ocultar os formulários
         $(function() {
             $("#btnEsqueci").click(function() {

a271a7cff1349c68a880cea9e6e549f18c90cb32
rafaeldaro <rafael_c_castro@estudante.sc.senai.br>
2019-09-30 21:55:15 -0300

    mostrar e ocultar paineis

--- index.php
+++ index.php
@@ -16,10 +16,11 @@
     <title>Sistema de login</title>
 
     <style>
-    #alerta, #caixaSenha, #caixaRegistro{
-        display: none;
-    }
-    
+        #alerta,
+        #caixaSenha,
+        #caixaRegistro {
+            display: none;
+        }
     </style>
 
 </head>
@@ -202,6 +203,33 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+        //código jQuery para mostrare ocultar os formulários
+        $(function() {
+            $("#btnEsqueci").click(function() {
+                $("#caixaSenha").hide(); //ocultar
+                $("#caixaLogin").show(); //
+            });
+
+            $("#btnRegistrarNovo").click(function() {
+                $("#caixaLogin").hide(); //ocultar
+                $("#caixaSenha").show(); //
+
+            });
+
+            $("#btnJaRegistrado").click(function() {
+                $("#caixaSenha").hide(); //ocultar
+                $("#caixaRegistro").show(); //
+
+            });
+            $("#btnJaRegistrado2").click(function() {
+                $("#caixaLogin").hide(); //ocultar
+                $("#caixaRegistro").show(); //
+
+            });
+        });
+    </script>
+
 </body>
 
 </html>

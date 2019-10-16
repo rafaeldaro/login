d2fa8275b7d6e459dee72c8d58bacbfdc8fe9a21
rafaeldaro <rafael_c_castro@estudante.sc.senai.br>
2019-10-15 21:56:00 -0300

    fim de aula

--- recebe_dados.php
+++ recebe_dados.php
@@ -1,50 +1,92 @@
+
 <?php
-//CONECXAO COM O BANCO DE DADOS
+//Teste se existe a ação
 require_once 'configBD.php';
-
-
-function verificar_entrada($entrada){
+function verificar_entrada($entrada)
+{
     //filtrando a entrada
     $saida = htmlspecialchars($entrada);
     $saida = stripslashes($saida);
     $saida = trim($saida);
-    return $saida;//retorna a saida limopa
-
+    return $saida; //retorna a saida limpa
 }
-
-
-
-//Teste se existe a ação
-if(isset($_POST['action'])){
-
-
-    if($_POST['action'] == 'cadastro'){
-        //teste sw ação é igual a cadastro
-        echo"<p>cadastro</p>";
-        echo"\n<pre>";//pré formatar
+if (isset($_POST['action'])) {
+    //Teste se ação é igual a cadastro
+    if ($_POST['action'] == 'cadastro') {
+        //Teste se ação é igual a cadastro
+        # echo "\n<p>cadastro</p>";
+        # echo "\n<pre>";
+        # print_r($_POST);
+        # echo "\n</pre";
+        $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
+        $nomeDoUsuario = verificar_entrada($_POST['nomeDoUsuario']);
+        $emailUsuario = verificar_entrada($_POST['emailUsuario']);
+        $senhaDoUsuario = verificar_entrada($_POST['senhaDoUsuario']);
+        $senhaUsuarioConfirmar = verificar_entrada($_POST['senhaUsuarioConfirmar']);
+        $dataCriado = date("Y-m-d"); //data atual no formato banco de dados
+        //codificando as senhas
+        $senhaCodificada = sha1($senhaDoUsuario);
+        $senhaConfirmarCod = sha1($senhaUsuarioConfirmar);
+        //teste de captura de dados
+        // echo "<p>Nome Completo: $nomeCompleto </p>";
+        // echo "<p>Nome de Usuário: $nomeDoUsuario </p>";
+        // echo "<p> E-mail : $emailUsuario </p>";
+        // echo "<p>Senha : $senhaCodificada </p>";
+        // echo "<p>Data de Criação : $dataCriado </p>";
+        if ($senhaCodificada != $senhaConfirmarCod) {
+            echo "<p class='text-danger' >Senhas não conferem.</p>";
+            exit;
+        } else {
+            //as senhas conferem, verifica se o usuario ja existe no banco de dados
+            $sql = $connect->prepare("SELECT nomeDoUsuario, emailUsuario 
+            FROM usuario WHERE nomeDoUsuario = ? OR emailUsuario = ?");
+            $sql->bind_param("ss", $nomeDoUsuario, $emailUsuario);
+            $sql->execute();
+            $resultado = $sql->get_result();
+            $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+            //verificando a existencia do usuario no banco
+            if ($linha['nomeDoUsuario'] == $nomeDoUsuario) {
+                echo "<p class='text-danger'>Usuario indisponivel </p>";
+            } elseif ($linha['emailUsuario'] == $emailUsuario) {
+                echo "<p class='text-danger'>e-mail indisponivel </p>";
+            } else {
+                //usuario pode ser cadastrado no banco de dados
+                $sql = $connect->prepare("INSERT into usuario (nomeDoUsuario,
+            nomeCompleto, emailUsuario, senhaDoUsuario, dataCriado)
+            values(?, ?, ?, ?, ?)");
+                $sql->bind_param(
+                    "sssss",
+                    $nomeDoUsuario,
+                    $nomeCompleto,
+                    $emailUsuario,
+                    $senhaCodificada,
+                    $dataCriado
+                );
+                if ($sql->execute()) {
+                    echo "<p class='text-success'> Usuario Cadastrado</p>";
+                } else {
+                    echo "<p class='text-danger'> Usuario nao cadastrado </p>";
+                    echo "<p class='text-danger'> Algo deu Muito errado </p>";
+                }
+            }
+        }
+    } else if ($_POST['action'] == 'login') {
+        //Senão, teste se ação é login
+        echo "\n<p>login</p>";
+        echo "\n<pre>";
         print_r($_POST);
-        echo"\n</pre>";
-
-    }else if($_POST['action'] == 'login'){
-        //senão, teste se ação é login
-        echo "<p>login</p>";
-        echo "\n<pre>";//pré formatar
+        echo "\n</pre";
+    } else if ($_POST['action'] == 'senha') {
+        //Senão,teste se ação é recuperar senha
+        echo "\n<p>senha</p>";
+        echo "\n<pre>";
         print_r($_POST);
-        echo "\n</pre>";
-
-    }else if($_POST['action'] == 'senha'){
-        //senão, teste se ação é recuperar semha
-        echo "<p>senha</p>";
-        echo "\n<pre>";//pré formatar
-        print_r($_POST);
-        echo "\n</pre>";
-    }else{
-
+        echo "\n</pre";
+    } else {
         header("location:index.php");
     }
-
-}else{
-        //redirecionado para index.php, negando o acesso a esse arquivo diretamente
-        header("location:index.php");
-
-    }
+} else {
+    //redirecionando para index.php, negando o acesso
+    //a esse arquivo diretamente 
+    header("location:index.php");
+}
--- receber.php
+++ receber.php
@@ -1,22 +1,32 @@
 <?php
-# Back-end
-if(isset)
-
-
-
-echo "\n<pre>\n";
-print_r($_GET);#Array;
-echo "\n</pre>\n";
-
-
-print("<strong>Nome:</strong>");
-print("$_GET[nomeCompleto]");
-
-print("<strong>E-mail:</strong>");
-print("$_GET[email]");
-
-print("<strong>Senha:</strong>");
-print("$_GET[senhaUsuário]");
-
-print("<strong>Data de Nascimento:</strong>");
-print("$_GET[dataNascimento]");
+#Back-end
+#if verifica se o método GET está enviando dados
+if (isset($_GET['nomeUsuario'])) {
+    echo "\n<h1>Envio de dados feito pelo método <em>GET</em></h1>";
+    echo "\n<pre>\n";
+    print_r($_GET); #Array
+    echo "\n</pre>\n";
+    print("<br><strong>Nome:</strong>");
+    print("$_GET[nomeUsuario]");
+    print("<br><strong>E-mail:</strong>");
+    print("$_GET[email]");
+    print("<br><strong>Senha:</strong>");
+    print("$_GET[senhaUsuario]");
+    print("<br><strong>Data de Nascimento:</strong>");
+    print("$_GET[dataNascimento]");
+}
+// Verifica se o metodo POST está enviando dados
+if (isset($_POST['nome'])) {
+    echo "\n<h1>Envio de dados feito pelo método <em>POST</em></h1>";
+    echo "\n<pre>\n";
+    print_r($_POST); #Array
+    echo "\n</pre>\n";
+    print("<br><strong>Nome:</strong>");
+    print("$_POST[nome]");
+    print("<br><strong>E-mail:</strong>");
+    print("$_POST[email]");
+    print("<br><strong>Senha:</strong>");
+    print("$_POST[senha]");
+    print("<br><strong>Data de Nascimento:</strong>");
+    print("$_POST[dataDeNascimento]");
+}

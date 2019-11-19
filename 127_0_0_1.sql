04ba2db1d63d6cb66dafe9b9aa7c1163b7d3d415
rafaeldaro <rafael_c_castro@estudante.sc.senai.br>
2019-11-18 22:18:54 -0200

    Update 127_0_0_1.sql
    update

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Out-2019 às 21:27
+-- Tempo de geração: 18-Nov-2019 às 22:14
 -- Versão do servidor: 10.3.16-MariaDB
 -- versão do PHP: 7.3.7
 
@@ -31,14 +31,26 @@ USE `senai`;
 -- Estrutura da tabela `usuario`
 --
 
+DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `idUsuario` int(10) UNSIGNED NOT NULL,
   `nomeCompleto` varchar(100) NOT NULL,
   `nomeDoUsuario` varchar(45) NOT NULL,
   `emailUsuario` varchar(45) NOT NULL,
   `senhaUsuario` char(40) NOT NULL,
-  `dataCriado` date NOT NULL
+  `dataCriado` date NOT NULL,
+  `urlImagem` varchar(200) NOT NULL,
+  `token` char(10) NOT NULL,
+  `tempoDeVida` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `nomeCompleto`, `nomeDoUsuario`, `emailUsuario`, `senhaUsuario`, `dataCriado`, `urlImagem`, `token`, `tempoDeVida`) VALUES
+(1, 'tutuco', 'tutuco', 'tutu@qasrwa', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-11-18', 'https://abrilveja.files.wordpress.com/2019/05/mundo-urso-pardo-20181227-002-copy-1.jpg', '', '2019-11-19 00:08:43'),
+(2, 'carlao', 'carlao', 'faf@sefwa', '5dd4ebdac62609c834f7768f02286b798bd82a38', '2019-11-18', 'https://gooutside-static-cdn.akamaized.net/wp-content/uploads/sites/3/2019/02/shutterstock_526658989.jpg', '', '2019-11-19 00:09:55');
 
 --
 -- Índices para tabelas despejadas
@@ -60,7 +72,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
